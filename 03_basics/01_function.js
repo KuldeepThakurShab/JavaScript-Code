3f44b603350e482319828345835f8b7fcd9d655c
KULDEEP TOMAR <98374974+KuldeepThakurShab@users.noreply.github.com>
2023-10-23 02:13:08 +0000

    Function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,26 @@
+function sayMyName(){
+    console.log("K");
+    console.log("U");
+    console.log("L");
+    console.log("D");
+    console.log("E");
+    console.log("E");
+    console.log("P");
+}
+
+// sayMyName();
+
+function addTwoNum(num1,num2){
+    // console.log(num1+num2);
+    let ans=num1+num2;
+    return ans;
+}
+
+const result=addTwoNum(3,5);
+// console.log("Result:",result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`;
+}
+
+console.log(loginUserMessage("Kuldeep"));
