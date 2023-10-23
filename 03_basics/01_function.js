0516869003f35f261fb024fbf76fce1f44947533
KULDEEP TOMAR <98374974+KuldeepThakurShab@users.noreply.github.com>
2023-10-23 03:44:54 +0000

    Function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -20,7 +20,48 @@ const result=addTwoNum(3,5);
 // console.log("Result:",result);
 
 function loginUserMessage(username){
+    if(!username){
+        console.log("Please enter the user name");
+        return ;
+    }
     return `${username} just logged in`;
 }
 
-console.log(loginUserMessage("Kuldeep"));
+// console.log(loginUserMessage("Kuldeep"));
+// console.log(loginUserMessage("Kuldeep"));
+
+//rest operator
+function calculateCartPrice(val1,val2, ...num1){
+    return num1;
+
+}
+
+// console.log(calculateCartPrice(200,400,500,2000));
+
+//Function with object 
+
+const user={
+    username:"kuldeep",
+    price:199
+}
+
+function HandleObj(anyObj){
+    console.log(`Username is ${anyObj.username} and price is ${anyObj.price}`)
+}
+
+// HandleObj(user);
+HandleObj({
+    username:"sam",
+    price:199
+})
+
+//function with Array
+
+const myNewArray=[200,4000,7000]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,900,300]));
